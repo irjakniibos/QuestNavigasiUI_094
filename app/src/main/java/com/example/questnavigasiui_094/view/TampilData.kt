12357df20bbe3a9ec048259b857f711eeb81fbb0
irjakniibos <irza.yaumil.ft23@mail.umy.ac.id>
2025-11-06 13:23:50 +0700

    Menambahkan Scaffold dan TopAppBar dengan tema warna

--- app/src/main/java/com/example/questnavigasiui_094/view/TampilData.kt
+++ app/src/main/java/com/example/questnavigasiui_094/view/TampilData.kt
@@ -1,13 +1,34 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.example.questnavigasiui_094.view
 
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import com.example.questnavigasiui_094.R
 
 @Composable
 fun TampilData(
     jenisK:List<String> = listOf("laki-laki","perempuan"),
     OnSubmitBtnClick : () -> Unit
 ){
+    Scaffold(modifier = Modifier,
+        {
+            TopAppBar(
+                title = {Text(stringResource(id= R.string.home),
+                    color = Color.White)},
+                colors = TopAppBarDefaults.topAppBarColors(
+                    colorResource(id = R.color.teal_700)
+                )
+            )
+        }){ isiRuang ->
 
+    }
 }
