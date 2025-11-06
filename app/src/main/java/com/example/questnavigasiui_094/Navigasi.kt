f0c575b8b2aeb7a582707ee5a16aa08a7a1b5406
irjakniibos <irza.yaumil.ft23@mail.umy.ac.id>
2025-11-06 12:36:26 +0700

    Menambahkan Implementasi navigasi dasar FormIsian ke Detail

--- app/src/main/java/com/example/questnavigasiui_094/Navigasi.kt
+++ app/src/main/java/com/example/questnavigasiui_094/Navigasi.kt
@@ -1,2 +1,38 @@
 package com.example.questnavigasiui_094
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+enum class Navigasi{
+    Formulirku,
+    Detail
+}
+
+@Composable
+fun DataApp(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier
+){
+    Scaffold { isiRuang->
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.Formulirku.name,
+
+            modifier = Modifier.padding( isiRuang)){
+            composable(route = Navigasi.Formulirku.name){
+                FormIsian (
+                    //pilihanJK = JenisK.map {id -> konteks.resources.getString
+                    OnSubmitBtnClick = {
+                        navController.navigate(Navigasi.Detail.name)
+                    }
+                )
+            }
+        }
+    }
+}
